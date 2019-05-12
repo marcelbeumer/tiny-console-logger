1233c26229d3ac9f693e06a90a9aecb8c67f3e0a
Marcel Beumer <marcel@marcelbeumer.com>
2019-05-12 22:43:58 +0200

    Let !format and !report throw on runtime

--- index.ts
+++ index.ts
@@ -43,7 +43,7 @@ export default (): Logger => {
   const createMethod = (methodName: keyof LoggingMethods, logLevel: LogLevel) => {
     const levelIndex = levelOrder.indexOf(logLevel);
     return (...args: any[]) => {
-      if (!format || !report || currentLevelIndex > levelIndex) return;
+      if (currentLevelIndex > levelIndex) return;
       report(format(args, methodName, logLevel), methodName, logLevel);
     };
   };
